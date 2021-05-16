12d4ba0159cda5919d8af544a06ac35240dedddf
jocavuleta <jocavuleta@email.com>
2021-05-17 00:15:52 +0200

    Initial commit

---
+++ src/main/java/com/example/game/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.annotation.PostConstruct;
+import java.util.*;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
